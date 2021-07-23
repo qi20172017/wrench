0c9b11f101eb5b28480c569d5e4f70f6ba5dae9d
qifumin <fumin.qi@aisuperstar.com>
2021-07-24 00:42:09 +0800

    init

---
+++ wrench/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File  : __init__.py.py
+@Author: crawlSpider
+@Address: https://weixin.sogou.com/weixin?type=1&s_from=input&query=%E7%BD%91%E8%99%ABspider&ie=utf8&_sug_=n&_sug_type_=
+@Date  : 2021/7/24 上午12:40
+@Desc  : 
+"""
+
+if __name__ == '__main__':
+    pass
